7ff9aa78341f383bcff152662323fa37c94bb19c
Yuri Savonik <62645573+Savonyk@users.noreply.github.com>
2022-12-09 03:36:36 +0200

    Added the normals

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let shProgram;                  // A shader program
 let spaceball;                  // A SimpleRotator object that lets the user rotate the view by mouse.
 let height = 1.5;
 let width = 360;
-let u = 19;
+let u = 18;
 let v = 0.15;
 let p = 1;
 
@@ -14,17 +14,89 @@ function GetRadiansFromDegree(angle) {
     return angle * Math.PI / 180;
 }
 
+function GetNormal(firstPoint, secondPoint, thirdPoint)
+{
+    let result = []; 
+
+    let a = m4.subtractVectors(secondPoint,firstPoint);
+    let b = m4.subtractVectors(thirdPoint,firstPoint);
+
+    result = m4.cross(a, b);
+
+    return result;
+}
+
+function CreateTriangles(vertices)
+{
+    let triangles = [];
+
+    for (let i = 0; i < vertices.length; i += 3) {
+
+        let first = [vertices[i], vertices[i + 1], vertices[i + 2]];
+        let second = [vertices[i + 3], vertices[i + 4], vertices[i + 5]];
+        let third = [vertices[i + 6], vertices[i + 7], vertices[i + 8]];
+
+        triangles.push(new Triangle(first, second, third, m4.normalize(GetNormal(first, second, third))));
+    }
+    return triangles;
+}
+
+function UpdateNormalsInTriangles(triangles)
+{
+    for(let i = 0; i < triangles.length - 1;i++)
+    {
+        for(let j = i + 1; j < triangles.length; j++)
+        {
+            if(triangles[i].ContainsVertex(triangles[j]))
+            {
+                let normal = m4.addVectors(triangles[i].normal, triangles[j].normal);
+                triangles[i].normal = normal;
+            }
+            
+        }
+    }
+}
+
+function IsEqualValue(first, second)
+{
+    if(first[0] == second[0] || first[1] == second[1] || first[2] == second[2])     return true;
+
+    return false;
+}
+
+function Triangle(first, second, third, normal)
+{
+    this.firstPoint = first;
+    this.secondPoint = second;
+    this.thirdPoint = third;
+    this.normal = normal;
+    this.vertexs = [];
+
+    this.ContainsVertex = function(triangle)
+    {
+        if(this == triangle) return false;
+
+        if(IsEqualValue(this.firstPoint, triangle.firstPoint) || IsEqualValue(this.secondPoint, triangle.secondPoint)
+          || IsEqualValue(this.thirdPoint, triangle.thirdPoint)) return true;
+
+        return false;
+    }
+}
 
 // Constructor
 function Model(name) {
     this.name = name;
     this.iVertexBuffer = gl.createBuffer();
+    this.iNormalBuffer = gl.createBuffer();
     this.count = 0;
 
-    this.BufferData = function(vertices) {
+    this.BufferData = function(vertices, normal) { 
 
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normal), gl.STREAM_DRAW);
 
         this.count = vertices.length/3;
     }
@@ -34,20 +106,25 @@ function Model(name) {
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(shProgram.iAttribVertex);
-   
+
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.count);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribNormal, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribNormal);
     }
 }
 
-
 // Constructor
-function ShaderProgram(name, program) {
+function ShaderProgram(name, program) { 
 
     this.name = name;
     this.prog = program;
 
     // Location of the attribute variable in the shader program.
     this.iAttribVertex = -1;
+    // Location of the attribute variable in the shader program.
+    this.iAttribNormal = -1;
     // Location of the uniform specifying a color for the primitive.
     this.iColor = -1;
     // Location of the uniform matrix representing the combined transformation.
@@ -68,7 +145,7 @@ function draw() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     
     /* Set the values of the projection transformation */
-    let projection = m4.perspective(Math.PI/8, 1, 8, 12); 
+    let projection = m4.orthographic(-2, 2, -2,2, 8, 12); 
     
     /* Get the view matrix from the SimpleRotator object.*/
     let modelView = spaceball.getViewMatrix();
@@ -85,7 +162,6 @@ function draw() {
 
     gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection );
     
-    /* Draw the six faces of a cube, with different colors. */
     gl.uniform4fv(shProgram.iColor, [0.3,0,1,1] );
 
     surface.Draw();
@@ -103,17 +179,36 @@ function CreateSurfaceData()
 {
     let vertexList = [];
 
-    for (let i = 0; i <= 360; i += u) {
-        for(let j = -height; j <= height; j += v){
+    for (let i = 0; i <= 360; i += u) 
+    {
+        for(let j = -height; j <= height; j += v)
+        {
             let currentAngle = GetRadiansFromDegree(i);
             let currentTemp = GetCurrentZPosition(j);
-            let nextAngle = GetRadiansFromDegree(i + u)
+            let nextAngle = GetRadiansFromDegree(i + u);
+
             vertexList.push(currentTemp * Math.cos(currentAngle), j, currentTemp * Math.sin(currentAngle));
             vertexList.push(currentTemp * Math.cos(nextAngle), j, currentTemp * Math.sin(nextAngle));
         }
     }
 
     return vertexList;
+}
+
+function CreateSurfaceNormals(vertices)
+{
+    let normalList = [];
+    let triangles = CreateTriangles(vertices);
+
+    UpdateNormalsInTriangles(triangles);
+
+    for(let i = 0; i < triangles.length; i++)
+    {
+        let normal = m4.normalize(triangles[i].normal);
+        normalList.push(normal[0], normal[1], normal[2]);
+    }
+
+    return normalList;
 }
 
 
@@ -125,11 +220,12 @@ function initGL() {
     shProgram.Use();
 
     shProgram.iAttribVertex              = gl.getAttribLocation(prog, "vertex");
+    shProgram.iAttribNormal            = gl.getAttribLocation(prog, "normal");
     shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
     shProgram.iColor                     = gl.getUniformLocation(prog, "color");
 
     surface = new Model('Surface');
-    surface.BufferData(CreateSurfaceData());
+    surface.BufferData(CreateSurfaceData(), CreateSurfaceNormals(CreateSurfaceData()));
 
     gl.enable(gl.DEPTH_TEST);
 }
